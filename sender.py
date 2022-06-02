c91d649d185900536fdaa298d1ba95cb47cab281
reannu123 <reannumon123@gmail.com>
2022-06-02 16:51:51 +0800

    Removed adjustment

--- sender.py
+++ sender.py
@@ -1,11 +1,8 @@
 # Create a transport protocol on top of UDP that reads from a file and sends the contents of the file to the client.
 import socket
 import argparse
-from time import sleep,time
-import hashlib
+from time import time
 
-def compute_checksum(packet):
-    return hashlib.md5(packet.encode('utf-8')).hexdigest()
 
 
 def udp_receive(clientSock):
@@ -63,14 +60,52 @@ def send_intent(intentMessage, clientSock, UDP_IP_ADDRESS, UDP_PORT_NO):
     while(True):
         try:
             udp_send(intentMessage, clientSock, UDP_IP_ADDRESS, UDP_PORT_NO)
-            # Measure the time it takes to receive the ACK
-            start = time()
+            # Measure the time it takes to receive the TxnID
             TxnID = udp_receive(clientSock)
-            end = time()
-            print(f"Time elapsed: {end-start}")
             return TxnID
         except socket.timeout:
             print("Timed out waiting for server")
+
+
+def send_packets(uniqueID,TxnID,message, clientSock, UDP_IP_ADDRESS, UDP_PORT_NO):
+    sequence_number = 0
+    idx = 0                         # Message index
+    chunkSize = 1                   # Initial Chunk size
+    
+    while idx < len(message):
+        """
+        Sending Packets
+        """
+        # Determine data parameters: submessage, isLast, seqNum
+        submessage = message[idx:idx+chunkSize]
+        sequence_number = 0 if sequence_number == 10000000 else sequence_number
+        isLast = 0 if idx+chunkSize < len(message) else 1
+
+        # Begin sending message 
+        data = f"ID{uniqueID}SN{str(sequence_number).zfill(7)}TXN{TxnID}LAST{isLast}{submessage}"
+        startTime = time()
+        udp_send(data, clientSock, UDP_IP_ADDRESS, UDP_PORT_NO)
+        idx+=chunkSize
+        sequence_number += 1
+
+
+        """
+        Receive ACK
+        """
+        # Receiving Loop
+        while(True):
+            try:
+                ack = udp_receive(clientSock)
+                endTime = time()
+                diff = endTime-startTime
+                if sequence_number == 1:
+                    clientSock.settimeout(diff+1)
+                    chunkSize = estimate_chunk_size(len(message), diff)
+                    break
+            except socket.timeout:
+                print("Timed out waiting for server")
+                if sequence_number > 1:
+                    chunkSize -=1
 
 
 def estimate_chunk_size (file_size, timeout):
@@ -79,6 +114,7 @@ def estimate_chunk_size (file_size, timeout):
     chunkSize = int(file_size/numofPackets)
     # Calculate the chunk size
     return chunkSize
+
 
 def main():
     args = get_args()
@@ -106,148 +142,17 @@ def main():
     # Send Intent message to server
     intentMessage  =f"ID{uniqueID}"
     TxnID = send_intent(intentMessage, clientSock, UDP_IP_ADDRESS, UDP_PORT_NO)
-    ProjectStart = time()
-    print("TxnID: "+TxnID)
-    print()
     if TxnID == "Existing alive transaction":
-        TxnID = read_file(f"txnID.txt")
         return
     else:
         write_file(f"txnID.txt", TxnID)
 
 
-
     """
     Send Packets
     """
-    sequence_number = 0
-    idx = 0                       # Message index
-    chunkSize = 1               # Anticipate accepted increment size
-    queueSize = 1               # Anticipate queue size
-    queue = 0
     
-    iChunkSize = chunkSize
-    isMeasuring = False
-    increaseFlag = False
-    decreaseFlag = False
-    packetSizes = [1,2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34,36,38,40,42,44,46,48,50,52,54,56,58,60,62,64,66,68,70,72,74,76]
-    packetSizes = [i for i in range(1,75)]
-    # packetSizes = [1,2,4,8,16,32,64,128]
-    packetSizeIdx = 0
-    minWrongSize = 99999
-
-    timeOuts = []
-    startTime = 0
-    while idx < len(message):
-        """
-        Sending Loop
-        """
-        
-
-        while queue<queueSize:
-            # Determine data parameters: submessage, isLast, seqNum
-            submessage = message[idx:idx+chunkSize]
-            sequence_number = 0 if sequence_number == 10000000 else sequence_number
-            isLast = 0 if idx+chunkSize < len(message) else 1
-            print(f"                                  PACKET {sequence_number}                      ")
-            print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-            # Begin sending message 
-            data = f"ID{uniqueID}SN{str(sequence_number).zfill(7)}TXN{TxnID}LAST{isLast}{submessage}"
-            print(f"Sqnc:\t\t{sequence_number}")
-            print(f"Size:\t\t{chunkSize}")
-            print("Packet:\t\t" + data)
-            startTime = time()
-            udp_send(data, clientSock, UDP_IP_ADDRESS, UDP_PORT_NO)
-            queue+=1
-            idx+=chunkSize
-            sequence_number += 1
-
-
-        """
-        Receive ACK
-        """
-        received = 0
-        receiveSize = queueSize
-        while received < receiveSize:
-            # Receiving Loop
-            while(True):
-                try:
-                    ack = udp_receive(clientSock)
-                    endTime = time()
-                    diff = endTime-startTime
-                    timeOuts.append(diff)
-                    
-                    """ 
-                    Experimental packet: 
-                        -begin measuring
-                        -set timeout from experiment
-                        -set Chunksize to default
-                        -set queue size to default
-                    """
-                    if sequence_number == 1:
-                        isMeasuring = True
-                        clientSock.settimeout(diff+1)
-                        iChunkSize = chunkSize
-                        chunkSize = estimate_chunk_size(len(message), diff)
-                        queueSize = 1
-                        break
-                    
-                    """
-                    Increase Packet size if okay (and was not decreased)
-                    """
-                    # Chunk size okay, can increase
-                    if isMeasuring:
-                        print("\nMEASURING: \tChunk size fine\n")
-                        iChunkSize = chunkSize
-                        newChunkSize = chunkSize + packetSizes[packetSizeIdx]
-                        while newChunkSize >=minWrongSize:
-                            if packetSizeIdx == 0:
-                                newChunkSize = chunkSize
-                                isMeasuring = False
-                                break
-                            packetSizeIdx-=1
-                            newChunkSize = chunkSize + packetSizes[packetSizeIdx]
-                        chunkSize = newChunkSize
-                        packetSizeIdx += 1
-                    break
-
-                except socket.timeout:
-                    endTime = time()
-                    print("~~~~Timed out waiting for server~~~~")
-                    
-                    # Chunk Size too Big, decrease
-                    if isMeasuring:
-                        minWrongSize = min(minWrongSize, chunkSize)
-                        if packetSizeIdx == 1:
-                            isMeasuring = False
-                        print("\nMEASURING: \tCHUNK SIZE TOO BIG\n")
-                        idx-=chunkSize*queueSize        # resend previous packet
-                        sequence_number -= queueSize
-                        packetSizeIdx = 0
-                        decreaseFlag = True
-                        if sequence_number == 1:
-                            chunkSize -= chunkSize//7
-                        else:
-                            chunkSize = (chunkSize+iChunkSize)//2
-                        
-                        break
-            
-            print(f"Rcv Time: \t{endTime-startTime}")
-            print("ACK:\t\t" + ack)
-            print(f"Elapsed:\t{startTime - ProjectStart}")
-            remainingPayloadSize = len(message) - idx
-            print("Remain: \t" + str(max(0,remainingPayloadSize)))
-            received += 1
-            queue = 0
-            print("--------------------------------------------------------------------------------")
-
-    # Print Final Transaction Metrics
-    print()
-    print("TOTAL TIME: \t"+str(time()-ProjectStart))
-    print("Ave Proc Time: \t" + str(sum(timeOuts)/len(timeOuts)))
-    print("TxnID: \t\t"+TxnID)
-    print("Max Size: \t"+str(chunkSize))
-    print("Payload Size: \t"+str(len(message)))
+    send_packets(uniqueID,TxnID,message, clientSock, UDP_IP_ADDRESS, UDP_PORT_NO)
 
 
 
